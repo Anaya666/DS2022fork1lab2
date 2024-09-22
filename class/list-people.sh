29b33fd307541665c77397f08c7b044a9030bb2c
Anaya666 <157422815+Anaya666@users.noreply.github.com>
2024-09-22 11:25:24 -0400

    adding this game now

---
+++ class/list-people.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#change directories into /people 
+cd ../people
+#look for this file in each dir 
+target_file="README.md"
+#loop through each dir and spit out first line of the file 
+for dir in $(find . -type d); do 
+  if [ -f "$dir/$target_file" ]; then  
+    #echo $dir/$target_file;
+    name='head -n 1 "$dir/$target_file"';
+    echo $name;
+  fi
+done
+ 
